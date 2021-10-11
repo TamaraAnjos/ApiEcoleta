b5bcb6a81035240a2902f465b9cff94d8bdd9905
Tamara <73923747+TamaraAnjos@users.noreply.github.com>
2021-10-10 23:50:58 -0300

    Update CooperativaController.php

--- app/Http/Controllers/CooperativaController.php
+++ app/Http/Controllers/CooperativaController.php
@@ -143,6 +143,10 @@ class CooperativaController extends Controller
 
         $cooperativas = Cooperativa::select(Cooperativa::raw('*, SQRT(
             POW(69.1 * (-23.6491) * COS( 57.3), 2)) AS distance'))
+            ->havingRaw('distance < ?', [10])
+            ->orderBy('distance', 'ASC')
+            ->offset($offset)
+            ->limit(5)
             ->get();
 
         foreach($cooperativas as $bkey =>$bvalue) {
