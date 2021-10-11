08bf82b3e3b89dd65f656581ec31c2acb6949cf5
Tamara <73923747+TamaraAnjos@users.noreply.github.com>
2021-10-10 21:51:49 -0300

    Update CooperativaController.php

--- app/Http/Controllers/CooperativaController.php
+++ app/Http/Controllers/CooperativaController.php
@@ -141,13 +141,9 @@ class CooperativaController extends Controller
             $city = 'Embu das Artes';
         }
 
-        $cooperativas = Cooperativa::select(Cooperativa::raw('*, SQRT(
-            POW(69.1 * (latitude - '.$lat.'), 2) +
-            POW(69.1 * ('.$lng.' - longitude) * COS(latitude / 57.3), 2)) AS distance'))
-            ->havingRaw('distance < ?', [10])
-            ->orderBy('distance', 'ASC')
-            ->offset($offset)
-            ->limit(5)
+        $cooperativas = Cooperativa::select(Cooperativa::raw(SQL: select *, SQRT(
+            POW(69.1 * (latitude - -23.6491), 2) +
+            POW(69.1 * (-46.8526 - longitude) * COS(latitude / 57.3), 2)) AS distance from &quot;cooperativas&quot; having distance &lt; 10 order by &quot;distance&quot; asc limit 5 offset 0)
             ->get();
 
         foreach($cooperativas as $bkey =>$bvalue) {
