da41400eaefa9e6f4ffe11b6faa4c153291f0494
Tamara <73923747+TamaraAnjos@users.noreply.github.com>
2021-10-10 22:50:54 -0300

    Update CooperativaController.php

--- app/Http/Controllers/CooperativaController.php
+++ app/Http/Controllers/CooperativaController.php
@@ -142,7 +142,7 @@ class CooperativaController extends Controller
         }
 
         $cooperativas = Cooperativa::select(Cooperativa::raw('*, SQRT(
-            POW(69.1 * (-23.6491) * COS( 57.3), 2)) AS distance' &quot having distance &lt 10 order by &quot distance&quot asc limit 5 offset 0))
+            POW(69.1 * (-23.6491) * COS( 57.3), 2)) AS distance' distance &lt 10 order by &quot distance&quot asc limit 5 offset 0))
 
         foreach($cooperativas as $bkey =>$bvalue) {
             $cooperativas[$bkey]['avatar'] = url('media/avatars/'.$cooperativas[$bkey]['avatar']);
