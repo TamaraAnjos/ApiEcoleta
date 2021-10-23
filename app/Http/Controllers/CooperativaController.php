9c7ff6937cfc067a6216cf5e04fc7f4b2ac190ec
Tamara <73923747+TamaraAnjos@users.noreply.github.com>
2021-10-23 10:18:08 -0300

    Update CooperativaController.php

--- app/Http/Controllers/CooperativaController.php
+++ app/Http/Controllers/CooperativaController.php
@@ -146,7 +146,7 @@ class CooperativaController extends Controller
             ->limit(5)
             ->get();
 */
-        $Cooperativa = intval(integerValue());
+       $cooperativas = intval(stringValue());
         $cooperativas = Cooperativa::select(Cooperativa::raw('*, SQRT(
             POW(69.1 * (latitude - '.$lat.'), 2) +
             POW(69.1 * ('.$lng.' - longitude) * COS(latitude / 57.3), 2)) AS distance'))
